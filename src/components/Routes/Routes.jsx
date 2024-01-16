6168e803f17e1096a1adb35027237db17fe6bc8f
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2024-01-16 21:59:31 +0200

    create hero section

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+
 import Home from '../Home/Home';
 
-export const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route index element={<Home />} />
-    </Routes>
-  );
-};
+const AppRoutes = () => (
+  <Routes>
+    <Route index element={<Home />} />
+  </Routes>
+);
 
-export default Routes;
+export default AppRoutes;
